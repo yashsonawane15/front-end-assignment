5137cde047a14578fdf5297a18381cc7b7f32e29
Yash Sonawane <yashsonawane15@gmail.com>
2022-05-28 22:14:46 +0530

    BookShow working

--- src/screens/bookshow/BookShow.js
+++ src/screens/bookshow/BookShow.js
@@ -13,7 +13,7 @@ import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
 import FormHelperText from "@material-ui/core/FormHelperText";
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
 
 const BookShow = (props) => {
 
