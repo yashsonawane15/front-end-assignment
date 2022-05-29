121ea2338a6cf6bafca3e715a4427b50d8809299
Yash Sonawane <yashsonawane15@gmail.com>
2022-05-29 11:01:36 +0530

    Error correction

---
+++ src/screens/home/ReleasedMoviesComponent.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { GridList, GridListTile, GridListTileBar } from "@material-ui/core";
+import { useHistory } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles'
+
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper
+    },
+    gridListMain: {
+        transform: 'translateZ(0)',        
+    },
+    releasedMovieGridItem: {
+        margin: '15px',
+        cursor: 'pointer'
+    },
+    title: {
+        color: theme.palette.primary.light,
+    }
+});
+
+
+const ReleasedMoviesComponent = (props) => {
+
+    const history = useHistory();
+    const releasedMovies = props.releasedMovies;
+    const movieClickHandler = (movieId) => {
+
+        history.push('/movie/' + movieId);
+    }
+    
+    const { classes } = props;
+
+    return (
+        <div>
+            <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
+                {
+                    releasedMovies.map(movie => (
+                        <GridListTile onClick={() => movieClickHandler(movie.id)} className={classes.releasedMovieGridItem} key={"grid" + movie.id}>
+                            <img src={movie.poster_url} className="moviePoster" alt={movie.title} />
+                            <GridListTileBar
+                                title={movie.title}
+                                subtitle={<span>Release Date: {new Date(movie.release_date).toDateString()}</span>}
+                            />
+                        </GridListTile>
+                    ))
+                }
+            </GridList>
+        </div>
+    )
+}
+
+export default withStyles(styles)(ReleasedMoviesComponent);
